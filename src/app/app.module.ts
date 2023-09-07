fd3e74066e077f03e05db1922e317084b9dcabb7
Ezequiel Lino <sonisombra@gmail.com>
2023-09-07 11:20:31 -0600

    pipe fecha a toda la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,6 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+
+// Configuracion del locale de la app
+import localeEsMx from '@angular/common/locales/es-MX';
+import localeFRCA from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEsMx);
+registerLocaleData(localeFRCA);
 
 @NgModule({
   declarations: [
@@ -18,7 +25,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-MX'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
