6c631f949c8124da236dabe5e110c737e056c007
Ezequiel Lino <sonisombra@gmail.com>
2023-09-11 11:43:33 -0600

    pipes personalizados

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,6 +5,7 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   declarations: [],
@@ -15,6 +16,7 @@ import { PanelModule } from 'primeng/panel';
     FieldsetModule,
     MenubarModule,
     PanelModule,
+    ToolbarModule,
   ]
   
 })
