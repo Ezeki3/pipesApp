c86c7296fa331e355a3c05f0838c59b26a036c7a
Ezequiel Lino <sonisombra@gmail.com>
2023-09-11 23:34:07 -0600

    pipes personalizados de volar o no volar

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
+import { SortEvent } from 'primeng/api';
 
 @Component({
   selector: 'products-order',
@@ -9,13 +11,59 @@ export class OrderComponent implements OnInit {
 
   public isUpperCase: boolean = false;
 
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black
+    },
+    {
+      name: 'Daredevil',
+      canFly: false,
+      color: Color.red
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.red
+    },
+    {
+      name: 'Linterna Verde',
+      canFly: true,
+      color: Color.green
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+   
   }
 
   toggleUpperCase():void{
     this.isUpperCase = !this.isUpperCase;
   }
+
+  customSort(event: Hero) {
+    console.log(event)
+    this.heroes.sort((a:any, b:any) => {
+        let value1 = a[event.name];
+        let value2 = b[event.name];
+        let result = null;
+
+        if (value1 == null && value2 != null) result = -1;
+        else if (value1 != null && value2 == null) result = 1;
+        else if (value1 == null && value2 == null) result = 0;
+        else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
+        else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+
+        return event.color * result;
+    });
+}
 
 }
